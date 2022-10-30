e13d7518ec594a365d69616037f251dbe5b38928
zechariah popoola <olabode.oritogun@gmail.com>
2022-10-30 18:27:30 +0100

    more styling added from the header to the main section

--- src/App.js
+++ src/App.js
@@ -10,43 +10,64 @@ function App() {
         alt="share-button"
       />
       </div>
+
+      <div className="share-img2">
+      <img
+        id="share-button-two"
+        src="image\Type=Share, State=Default.png"
+        alt="share-button"
+      />
+      </div>
       <div className="header-img">
-        <p>Annette Black</p>
       <img
         id="profile__img"
         src="/image\Device=Web, State=Default.png"
         alt="profile-avi"
       />
       </div>
+      <p id="twitter">Labodinhoo</p>
+
+      {/* <p id="slack">Dinho</p> */}
       
       <div className="App-link">
-        <a href="https://www.google.com">
+        <a href="https://twitter.com/labodinhoo">
           {" "}
           <button>Twitter Link</button>
         </a>
-        <a href="https://www.google.com">
+        <a id="btn__zuri" href="https://training.zuri.team/">
           {" "}
           <button>Zuri Team</button>
         </a>
-        <a href="https://www.google.com">
+        <a id="books" href="http://books.zuri.team ">
           {" "}
           <button>Zuri Books</button>
         </a>
-        <a href="https://www.google.com">
+        <a id="book__python" href="https://books.zuri.team/python-for-beginners?ref_id=Dinho">
           {" "}
           <button>Python Books</button>
         </a>
-        <a href="https://www.google.com">
+        <a id="pitch" href="https://background.zuri.team">
           {" "}
           <button>Background Check for Coders</button>
         </a>
-        <a href="https://www.google.com">
+        <a id="book__design" href="https://books.zuri.team/design-rules">
           {" "}
           <button>Design Books</button>
         </a>
       </div>
 
-      
+      <div className="social-icons">
+        <img src="/image/slack.png" alt="slack logo"/>
+        <img src="/image/Icon.png" alt="github logo"/>
+      </div>
+
+      <hr />
+
+      <div className="footer-section">
+      <img src="/image/Zuri.Internship_Logo.png" alt="github logo"/>
+      <p>HNG Internship 9 Frontend Task</p>
+      <img src="image/I4G.png" alt="github logo"/>
+      </div>
     </div>
   );
 }
