7e4c0b2dcb8d35a7abbbe708348811ce851534df
zechariah popoola <olabode.oritogun@gmail.com>
2022-10-30 17:13:59 +0100

    links are added

--- src/App.js
+++ src/App.js
@@ -19,6 +19,32 @@ function App() {
       />
       </div>
       
+      <div className="App-link">
+        <a href="https://www.google.com">
+          {" "}
+          <button>Twitter Link</button>
+        </a>
+        <a href="https://www.google.com">
+          {" "}
+          <button>Zuri Team</button>
+        </a>
+        <a href="https://www.google.com">
+          {" "}
+          <button>Zuri Books</button>
+        </a>
+        <a href="https://www.google.com">
+          {" "}
+          <button>Python Books</button>
+        </a>
+        <a href="https://www.google.com">
+          {" "}
+          <button>Background Check for Coders</button>
+        </a>
+        <a href="https://www.google.com">
+          {" "}
+          <button>Design Books</button>
+        </a>
+      </div>
 
       
     </div>
