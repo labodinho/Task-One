f5004983870759aabea6da0fe17b8712ffa6a801
zechariah popoola <olabode.oritogun@gmail.com>
2022-10-30 17:09:18 +0100

    header images added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import "./App.css";
 function App() {
   return (
     <div>
-     
+     <div className="share-img">
+      <img
+        id="share-button"
+        src="/image/_Avatar share button.png"
+        alt="share-button"
+      />
+      </div>
+      <div className="header-img">
+        <p>Annette Black</p>
+      <img
+        id="profile__img"
+        src="/image\Device=Web, State=Default.png"
+        alt="profile-avi"
+      />
+      </div>
+      
+
+      
     </div>
   );
 }
