a885b61548adbb60d86d82c14ce01a8013d436c6
zechariah popoola <olabode.oritogun@gmail.com>
2022-10-30 22:17:13 +0100

    adjusted some styling and added media queries

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
 
       <div className="footer-section">
       <img src="/image/Zuri.Internship_Logo.png" alt="github logo"/>
-      <p>HNG Internship 9 Frontend Task</p>
+      <p className="HNG">HNG Internship 9 Frontend Task</p>
       <img src="image/I4G.png" alt="github logo"/>
       </div>
 
